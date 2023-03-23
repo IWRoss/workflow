a9970d565a780ff5c97c236255c11150dcd1f357
Connor Fadden <connorfadden@Rosss-MBP.localdomain>
2023-03-23 12:37:34 +0000

    Fixed minor reference error w/.text

--- controllers/slack.js
+++ controllers/slack.js
@@ -261,7 +261,7 @@ const claimTask = async (payload) => {
 
     // Send confirmation message
     const claimer = payload.user.id;
-    const poster = payload.message.blocks[0];
+    const poster = payload.message.blocks[0].text;
 
     // Use Regex to get the user id from the first block
     const regex = /<@(.*)>/;

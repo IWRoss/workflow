d368409ad4bceaa2343bbee224534f7420f28510
Ross Hardy <rosstavo@gmail.com>
2024-04-02 13:26:32 +0100

    Added imports to templates

--- templates/index.js
+++ templates/index.js
@@ -4,10 +4,13 @@
 const appHome = require("./appHomeScreen");
 const requestModal = require("./requestModal");
 const newRequestMessage = require("./newRequestMessage");
+const invoiceRequestModal = require("./invoiceRequestModal");
+const newInvoiceRequestMessage = require("./newInvoiceRequestMessage");
 
 module.exports = {
   appHome,
   requestModal,
   invoiceRequestModal,
   newRequestMessage,
+  newInvoiceRequestMessage,
 };
